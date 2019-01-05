96b66e5d95436c4fdc7c8961f4b2c281e58daab1
Dan Krishnan <dan.krishnan01@gmail.com>
2019-01-05 18:07:09 +0000

    added projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        currentYear: new Date().getFullYear()
+    })
+});
 
 app.get('/bad', (req, res) => {
     res.send({
